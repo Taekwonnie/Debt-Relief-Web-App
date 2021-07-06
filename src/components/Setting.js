bda267b8e1d52303d2d65e0779c0449f5a9fc51e
Jayden To <jaydento@my.unt.edu>
2021-07-05 21:00:09 -0500

    Update Setting.js

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 import {
   Button,
-  TextField,
-  Tooltip,
   Select,
   MenuItem,
   makeStyles,
+  FormControlLabel,
   Card,
   Switch,
   FormControl,
+  Box,
   FormHelperText,
   CardContent,
 } from "@material-ui/core";
@@ -25,16 +25,25 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Setting() {
-  const [currency, setCurrency] = useState("");
+  const [loading, setLoading] = useState(false); //For button
+
+  const [state, setState] = React.useState({
+    //For check box
+    checkedA: true,
+    checkedB: true,
+  });
+  const [currency, setCurrency] = useState(""); //For currency
+
   const handleChange = (event) => {
+    //For changes
     setCurrency(event.target.value);
+    setState({ ...state, [event.target.name]: event.target.checked });
   };
   const classes = useStyles();
 
   return (
     <div>
       <Sidebar />
-
       <Card>
         <CardContent>
           <h2 className="text-center mb-4">Setting</h2>
@@ -52,7 +61,52 @@ export default function Setting() {
             <FormHelperText className="w-100 text-justify mt-2">
               Set your currency
             </FormHelperText>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={state.checkedA}
+                  onChange={handleChange}
+                  name="checkedA"
+                />
+              }
+              label="Receive communication from Debt Relief."
+            />
           </FormControl>
+          <Button
+            normal
+            color="primary"
+            size="large"
+            type="submit"
+            variant="contained"
+            disabled={loading}
+            className="w-100 text-center mt-2"
+          >
+            Save Settings
+          </Button>
+          <Box display="flex" justifyContent="space-between">
+            <Button
+              normal
+              color="primary"
+              size="large"
+              type="submit"
+              variant="contained"
+              disabled={loading}
+              className="mt-2 mr-3"
+            >
+              Import Settings
+            </Button>
+            <Button
+              normal
+              color="primary"
+              size="large"
+              type="submit"
+              variant="contained"
+              disabled={loading}
+              className="mt-2"
+            >
+              Export Settings
+            </Button>
+          </Box>
         </CardContent>
       </Card>
     </div>
