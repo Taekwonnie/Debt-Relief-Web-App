00a2f4dbd45a36830ba07606b5e6aed55bc5bee4
Jayden To <jaydento@my.unt.edu>
2021-07-19 14:01:54 -0500

    Update UpdateAccount.js

--- src/components/UpdateAccount.js
+++ src/components/UpdateAccount.js
@@ -5,8 +5,15 @@ import { Link, useHistory } from "react-router-dom";
 import Alert from "@material-ui/lab/Alert";
 import { Row, Col } from "react-bootstrap";
 import {
-  Grid, CardHeader, Avatar, Button, TextField, 
-  Tooltip, Link as MaterialLink, Card, CardContent
+  Grid,
+  CardHeader,
+  Avatar,
+  Button,
+  TextField,
+  Tooltip,
+  Link as MaterialLink,
+  Card,
+  CardContent,
 } from "@material-ui/core";
 import Sidebar from "./Sidebar";
 
@@ -19,7 +26,7 @@ export default function UpdateAccount() {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
   const { currentUser, logout } = useAuth();
-  
+
   async function handleLogout() {
     try {
       await logout();
@@ -62,146 +69,131 @@ export default function UpdateAccount() {
 
   return (
     <>
-    <Sidebar />
-    <Grid container
-      spacing={10}>
-    
-    <Row>
-    <Col>
-    <Grid item xs={12} sm={12} md={12}>
-    <Card style={{width:"100%", height:"375px"}}> 
-      <CardHeader 
-        avatar={
-          <Avatar aria-label="profile-pic">
-            
-          </Avatar>
-        }
-        titleTypographyProps={{variant:'h5'}}
-        title="Account Information"
-        />
-      <CardContent>
-        <div className="w-100 text-center mb-4">
-          <strong>Email: </strong> {currentUser.email}
-        </div>
-        
-      </CardContent>
-    </Card>
-    </Grid>
-    </Col>
-    
-    <Col>
-    <Grid item xs={12} sm={12} md={12}>
-    <Card style={{width:"280px", height:"375px"}}>
-      <CardHeader 
-        title="Update Account"
-      />
+      <Sidebar />
+      <Grid container spacing={10}>
+        <Row>
+          <Col>
+            <Grid item xs={12} sm={12} md={12}>
+              <Card style={{ width: "100%", height: "375px" }}>
+                <CardHeader
+                  avatar={<Avatar aria-label="profile-pic"></Avatar>}
+                  titleTypographyProps={{ variant: "h5" }}
+                  title="Account Information"
+                />
+                <CardContent>
+                  <div className="w-100 text-center mb-4">
+                    <strong>Email: </strong> {currentUser.email}
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Col>
 
-      <CardContent>
-        {error && <Alert severity="error">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <TextField
-              required
-              id="standard-required"
-              label="Email"
-              defaultValue=""
-              inputRef={emailRef}
-              className="w-100 text-center mt-2"
-            />
-          </div>
+          <Col>
+            <Grid item xs={12} sm={12} md={12}>
+              <Card style={{ width: "280px", height: "375px" }}>
+                <CardHeader title="Update Account" />
 
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Tooltip title="Leave blank if you don't want to change">
-              <TextField
-                id="standard-required"
-                label="Password"
-                defaultValue=""
-                type="password"
-                inputRef={passwordRef}
-                className="w-100 text-center mt-2"
-              />
-            </Tooltip>
-          </div>
+                <CardContent>
+                  {error && <Alert severity="error">{error}</Alert>}
+                  <Form onSubmit={handleSubmit}>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <TextField
+                        required
+                        id="standard-required"
+                        label="Email"
+                        defaultValue=""
+                        inputRef={emailRef}
+                        className="w-100 text-center mt-2"
+                      />
+                    </div>
 
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Tooltip title="Leave blank if you don't want to change">
-              <TextField
-                id="standard-required"
-                label="Confirm Password"
-                defaultValue=""
-                type="password"
-                inputRef={passwordConfirmRef}
-                className="w-100 text-center mt-2"
-              />
-            </Tooltip>
-          </div>
-          <Button
-            color="primary"
-            size="large"
-            variant="contained"
-            disabled={loading}
-            type="submit"
-            className="w-100 text-center mt-4"
-          >
-            Update
-          </Button>
-        </Form>
-      </CardContent>
-    </Card>
-    </Grid>
-    </Col>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Tooltip title="Leave blank if you don't want to change">
+                        <TextField
+                          id="standard-required"
+                          label="Password"
+                          defaultValue=""
+                          type="password"
+                          inputRef={passwordRef}
+                          className="w-100 text-center mt-2"
+                        />
+                      </Tooltip>
+                    </div>
 
-    <Col>
-    <Grid item xs={12} sm={12} md={12} spacing={10}>
-    <Card style={{width:"100%", height:"150px"}}>
-    <CardHeader 
-        title="Logout of Your Account"
-      />
-      <CardContent>
-        <Button
-          color="default"
-          size="large"
-          variant="contained"
-          type="submit"
-          className="w-100 text-center mt-2"
-          onClick={handleLogout}
-        >
-          Log Out
-        </Button>
-        
-      </CardContent>
-    </Card>
-    </Grid>
-    
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Tooltip title="Leave blank if you don't want to change">
+                        <TextField
+                          id="standard-required"
+                          label="Confirm Password"
+                          defaultValue=""
+                          type="password"
+                          inputRef={passwordConfirmRef}
+                          className="w-100 text-center mt-2"
+                        />
+                      </Tooltip>
+                    </div>
+                    <Button
+                      color="primary"
+                      size="large"
+                      variant="contained"
+                      disabled={loading}
+                      type="submit"
+                      className="w-100 text-center mt-4"
+                    >
+                      Update
+                    </Button>
+                  </Form>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Col>
 
-    <div className="w-100 text-center mt-2">
-      <MaterialLink component={Link} to="/">
-        Back to dashboard
-      </MaterialLink>
-    </div>
+          <Col>
+            <Grid item xs={12} sm={12} md={12} spacing={10}>
+              <Card style={{ width: "100%", height: "150px" }}>
+                <CardHeader title="Logout of Your Account" />
+                <CardContent>
+                  <Button
+                    color="default"
+                    size="large"
+                    variant="contained"
+                    type="submit"
+                    className="w-100 text-center mt-2"
+                    onClick={handleLogout}
+                  >
+                    Log Out
+                  </Button>
+                </CardContent>
+              </Card>
+            </Grid>
 
-    </Col>
-    </Row>
-    
-  </Grid>
-  </>
-);
+            <div className="w-100 text-center mt-2">
+              <MaterialLink component={Link} to="/">
+                Back to dashboard
+              </MaterialLink>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
+    </>
+  );
 }
